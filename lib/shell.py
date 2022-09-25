61af65f91543dcfcbefc4ce0a5c23464c1c8ca45
Daniel G <dangarmol@icloud.com>
2022-09-25 21:03:44 +0200

    Improve documentation

--- lib/shell.py
+++ lib/shell.py
@@ -1,18 +1,23 @@
+"""
+Static functions to run shell commands.
+"""
+
+
 from subprocess import Popen, PIPE
 
 
 def run_command(command: str) -> str:
     """
-    Runs the specified command on the current device. This can be useful to perform actions on the Raspberry Pi's when running the scripts on them directly.
+    Runs the specified command on the current device default shell.
 
     Arguments:
-        - `command` (str): The command to be run on the shell.
+        - `command` (str): Command to be run.
 
     Raises:
         - `SystemError`: If there is any content returned via stderr.
 
     Returns:
-        tuple (stdout, stderr): Returns a tuple with the contents of stdout and stderr as bytes after running the command.
+        str: Contents of stdout after running the command, decoded using current locale.
     """
 
     (out, err) = Popen(command, shell=True, stdout=PIPE, stderr=PIPE).communicate()
